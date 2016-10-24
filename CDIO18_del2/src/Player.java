7b9aef009376a2915ce79ecb6e6649a5709f95f8
troelslund <troelslund@troelss-mbp.eduroam.wireless.dtu.dk>
2016-10-24 16:06:06 +0200

    Player

--- CDIO18_del2/src/Player.java
+++ CDIO18_del2/src/Player.java
@@ -1,4 +1,17 @@
 
 public class Player {
+	
+	Boolean Playerturn;
+	
+	
+	
 
+	public Boolean getPlayerturn() {
+		return Playerturn;
+	}
+
+	public void setPlayerturn(Boolean playerturn) {
+		Playerturn = playerturn;
+	}
+	
 }
