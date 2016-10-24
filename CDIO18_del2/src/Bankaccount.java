a1028cf3843df573d08acd76ee8e5e3a3bd889b7
troelslund <troelslund@troelss-mbp.eduroam.wireless.dtu.dk>
2016-10-24 21:42:43 +0200

    Bankkonto

--- CDIO18_del2/src/Bankaccount.java
+++ CDIO18_del2/src/Bankaccount.java
@@ -1,18 +1,42 @@
 
 public class Bankaccount {
-	
-	private int sum;
 
-	public int getSum() {
+	private double sum;
+	private int id;
+
+	Bankaccount(){
+		this(0,0);
+	}
+
+
+	Bankaccount(int id){
+		this(0,id);
+	}
+
+	Bankaccount(double balance, int id){
+		this.sum = balance;
+		this.id = id;
+	}
+
+	public double getSum() {
 		return sum;
 	}
 
 	public void setSum(int sum) {
 		this.sum = sum;
 	}
-	
+
 	public void addSum(int x) {
 		this.sum = sum + x;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 
 }
