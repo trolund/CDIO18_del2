49395ef8c1673f003c9950c2207173e88ddaf999
troelslund <troelslund@troelss-mbp.eduroam.wireless.dtu.dk>
2016-10-28 09:50:19 +0200

    Update af Game controller

--- CDIO18_del2/src/Gamecontroller.java
+++ CDIO18_del2/src/Gamecontroller.java
@@ -11,17 +11,29 @@ import desktop_resources.GUI;
 public class Gamecontroller {
 	
 	
-	Player player1;
-	Player player2;
-	Dicecup cup;
-	Field[] fields;
+	private Player player1;
+	private Player player2;
+	private Dicecup cup;
 	
 	public static void main(String[] args) {
 		new Gamecontroller().go();
 	}
 	
-	private void go(){	
+	public void go(){	
 		cup = new Dicecup();
+		player1 = new Player(0,"Knud");
+		player2 = new Player(0,"Brian");
+		
+		Field[] fields = new Field[8];
+		fields[0] = new Street.Builder().setTitle("Start").build();
+		fields[1] = new Street.Builder().setTitle("Felt 1").setRent("600,-").build();
+		fields[2] = new Street.Builder().setTitle("Felt 2").setRent("600,-").build();
+		fields[3] = new Street.Builder().setTitle("Felt 3").setRent("600,-").build();
+		fields[4] = new Street.Builder().setTitle("Felt 4").setRent("600,-").build();
+		fields[5] = new Street.Builder().setTitle("Felt 5").setRent("600,-").build();
+		fields[6] = new Street.Builder().setTitle("Felt 6").setRent("600,-").build();
+		fields[7] = new Street.Builder().setTitle("Felt 7").setRent("600,-").setSubText("du er bare god").build();
+		GUI.create(fields);
 
 		Car car1 = new Car.Builder()
 				.typeRacecar()
@@ -37,88 +49,71 @@ public class Gamecontroller {
 				.secondaryColor(Color.RED)
 				.build();
 		
-		player1 = new Player(0,"Knud");
-		player2 = new Player(0,"Brian");
-		
 		GUI.addPlayer(player1.getName(), player1.account.getSum(),car1);
 		GUI.addPlayer(player2.getName(), player2.account.getSum(),car2);
 		
-		fields = new Field[8];
-		fields[0] = new Street.Builder().setTitle("Start").build();
-		fields[1] = new Street.Builder().setTitle("Felt 1").setRent("600,-").build();
-		fields[2] = new Street.Builder().setTitle("Felt 2").setRent("600,-").build();
-		fields[3] = new Street.Builder().setTitle("Felt 3").setRent("600,-").build();
-		fields[4] = new Street.Builder().setTitle("Felt 4").setRent("600,-").build();
-		fields[5] = new Street.Builder().setTitle("Felt 5").setRent("600,-").build();
-		fields[6] = new Street.Builder().setTitle("Felt 6").setRent("600,-").build();
-		fields[7] = new Street.Builder().setTitle("Felt 7").setRent("600,-").setSubText("du er bare god").build();
-		
-		GUI.create(fields);
+		System.out.println("setup done");
 		
 		update();
 	}
 	
 	public void update(){
 		while(true){
-			turn(player1);
 			GUI.showMessage("Player 1 tur.");
-			
-			cup.die1.roll();
-			cup.die2.roll();
-			
-			player1.account.addSum(cup.getSum());
-			
-			GUI.setDice(cup.die1.getValue(), cup.die2.getValue());
-			GUI.setBalance(player1.getName(), player1.account.getSum());
-			
-			
-			
-			turn(player2);
+			turn(player1);
 			GUI.showMessage("Player 2 tur.");
-			
-			cup.die1.roll();
-			cup.die2.roll();
-			
-			player2.account.addSum(cup.getSum());
-			
-			GUI.setDice(cup.die1.getValue(), cup.die2.getValue());
-			GUI.setBalance(player2.getName(), player2.account.getSum());
+			turn(player2);
 			}
 	}
 	
 	private void turn(Player p){
 		
+		cup.die1.roll();
+		cup.die2.roll();
+		
+		
 		 switch (cup.getSum()) {
 
-//		 case 2:  monthString = "February";
-//		 break;
-//		 case 3:  monthString = "March";
-//		 break;
-//		 case 4:  monthString = "April";
-//		 break;
-//		 case 5:  monthString = "May";
-//		 break;
-//		 case 6:  monthString = "June";
-//		 break;
-//		 case 7:  monthString = "July";
-//		 break;
-//		 case 8:  monthString = "August";
-//		 break;
-//		 case 9:  monthString = "September";
-//		 break;
-//		 case 10: monthString = "October";
-//		 break;
-//		 case 11: monthString = "November";
-//		 break;
-//		 case 12: monthString = "December";
-//		 break;
-//		 default: monthString = "Invalid month";
-//		 break;
-     }
-	
-		
-		
-		
+		 case 2: 
+			 p.account.addSum(600);
+		 break;
+		 case 3:
+			 p.account.addSum(600);
+		 break;
+		 case 4:
+			 p.account.addSum(600);
+		 break;
+		 case 5: 
+			 p.account.addSum(600);
+		 break;
+		 case 6: 
+			 p.account.addSum(600);
+		 break;
+		 case 7:  
+			 p.account.addSum(600);
+		 break;
+		 case 8: 
+			 p.account.addSum(600);
+		 break;
+		 case 9:  
+			 p.account.addSum(600);
+		 break;
+		 case 10: 
+			 p.account.addSum(600);
+		 break;
+		 case 11:
+			 p.account.addSum(600);
+		 break;
+		 case 12: 
+			 p.account.addSum(600);
+		 break;
+		 default: 
+			 p.account.addSum(600);
+		 break;
+
+     }	
+		 GUI.setBalance(p.getName(), p.account.getSum());
+		 GUI.setDice(cup.die1.getValue(), cup.die2.getValue());
 		
 	}
 
